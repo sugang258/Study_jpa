c1773d0087f2816918388a82005e49a0cdd3c910
Hongsugyeong <olivia258@naver.com>
2023-12-18 20:24:08 +0900

    [Feat] JPQL 추가

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
 
@@ -31,6 +32,14 @@ public class JpaMain {
 /*            DELETE
               em.remove();
  */
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .setFirstResult(5)
+                    .setMaxResults(8)
+                    .getResultList();
+
+            for(Member member : result) {
+                System.out.println(member.getName());
+            }
 
             tx.commit();
         }catch (Exception e) {
