25985965b1f882a264f86b6a9079c7a9968b5142
Hongsugyeong <olivia258@naver.com>
2023-12-19 17:34:42 +0900

    [Feat] 영속성 관리 공부

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -32,14 +32,56 @@ public class JpaMain {
 /*            DELETE
               em.remove();
  */
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(5)
-                    .setMaxResults(8)
-                    .getResultList();
+//            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+//                    .setFirstResult(5)
+//                    .setMaxResults(8)
+//                    .getResultList();
+//
+//            for(Member member : result) {
+//                System.out.println(member.getName());
 
-            for(Member member : result) {
-                System.out.println(member.getName());
-            }
+
+//            }
+
+//            //비영속
+//            Member member = new Member();
+//            member.setId(101L);
+//            member.setName("HelloJPA");
+
+//            //영속
+//            System.out.println("========BEFORE=======");
+//            em.persist(member); //1차 캐시에 저장
+//            System.out.println("========AFTER==========");
+
+//            Member findMember1 = em.find(Member.class, 101L);
+//            Member findMember2 = em.find(Member.class, 101L);
+
+//            Member member1 = new Member(150L, "A");
+//            Member member2 = new Member(160L, "B");
+//
+//            System.out.println("===================");
+//            em.persist(member1);
+//            em.persist(member2);
+
+
+//            Member member = new Member(200L,"member200");
+//            em.persist(member);
+//
+//            em.flush();
+
+            Member member = em.find(Member.class, 150L);
+            member.setName("AAAAA");
+
+            //영속 -> 준영속
+            em.detach(member); //특정 엔티티만
+            em.clear(); //완전히 초기화
+            em.close(); // 종료
+
+
+            System.out.println("===============");
+
+//            System.out.println("findMember.id = " + findMember.getId() );
+//            System.out.println("findMember.name = " + findMember.getName());
 
             tx.commit();
         }catch (Exception e) {
